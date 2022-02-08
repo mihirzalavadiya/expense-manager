664eeb2db3efd1645bf54b64e591221e80ceeeac
mihirzalavadiya <mihirzalavadiya@gmail.com>
2022-02-08 09:42:55 +0530

    use map

--- src/components/Expenses/Expenses.js
+++ src/components/Expenses/Expenses.js
@@ -8,9 +8,9 @@ const Expenses = (props) => {
     const { items } = props;
   return (
     <Card className="expenses">
-      <ExpenseItem expense={items[0]} />
-      <ExpenseItem expense={items[1]} />
-      <ExpenseItem expense={items[2]} />
+      {items.map((item) => (
+        <ExpenseItem key={item.id} expense={item} />
+      ))}
     </Card>
   );
 };
